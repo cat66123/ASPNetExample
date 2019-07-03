781666d2ba5965e74f3ee0f6efe777ccfe21458c
cat66123 <326700510@qq.com>
2019-07-03 11:58:41 +0800

    创建树

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+using WebApplication1.Logic.CreateTree;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Node> list = new List<Node>() {
+                new Node {id=1,name="a",parentid=null },
+                new Node {id=2,name="b",parentid=null},
+                new Node {id=3,name="c",parentid=1 },
+                new Node {id=4,name="d",parentid=null },
+                new Node {id=5,name="e",parentid=3 },
+                new Node {id=6,name="f",parentid=2 },
+                new Node {id=7,name="g",parentid=null }
+            };
+
+            List<NewNode> rlt = TreeFns.CreateNewTree(list);
+
+            ViewBag.Cat66 = rlt;
+
+            return View();
+        }
+
+  
+      
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Logic/CreateTree/Tree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Logic.CreateTree
+{
+    public class Node
+    {
+        public int id;
+        public string name;
+        public int? parentid;
+    }
+    public class NewNode
+    {
+        public int id;
+        public string name;
+        public int? parentid;
+        public List<NewNode> items;
+    }
+
+    public static class TreeFns
+    {
+        public static List<NewNode> CreateNewTree(List<Node> originalList)
+        {
+            List<NewNode> nodes = originalList.Where(v => v.parentid == null).Select(v => new NewNode() { id = v.id, name = v.name, parentid = v.parentid, items = new List<NewNode>() { } }).ToList();
+            foreach (NewNode node in nodes)
+            {
+                node.items = GetAllLeaves(node, originalList);
+            }
+            return nodes;
+        }
+
+        public static List<NewNode> GetAllLeaves(NewNode val, List<Node> originalList)
+        {
+            List<NewNode> nodes = originalList.Where(v => v.parentid == val.id).Select(v => new NewNode() { id = v.id, name = v.name, parentid = v.parentid, items = new List<NewNode>() { } }).ToList();
+            foreach (NewNode node in nodes)
+            {
+                node.items = GetAllLeaves(node, originalList);
+            }
+            return nodes;
+        }
+    }
+}
